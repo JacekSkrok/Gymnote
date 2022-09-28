77bdb330637ad3665fd152d1263fc994d51cc619
LazyJackyboy <skrok.jacek21@gmail.com>
2022-09-28 22:57:49 +0200

    added initialization and on onclic method of objects from registerView

--- app/src/main/java/com/application/gymnote/RegisterUser.java
+++ app/src/main/java/com/application/gymnote/RegisterUser.java
@@ -2,11 +2,21 @@ package com.application.gymnote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterUser extends AppCompatActivity {
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView banner, registerUser;
+    private EditText editTextName, editTextAge, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
 
@@ -17,6 +27,33 @@ public class RegisterUser extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.button2);
+        registerUser.setOnClickListener(this);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmailAdress);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.button2:
+                registerUser();
+                break;
+                
+        }
+    }
+
+    private void registerUser() {
+    }
 }
