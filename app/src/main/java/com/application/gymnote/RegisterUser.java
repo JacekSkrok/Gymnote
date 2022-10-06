873b8e56dda169897eafb7058accfa5b89c2e003
LazyJackyboy <skrok.jacek21@gmail.com>
2022-10-06 16:53:40 +0200

    added registry user with pass and email

--- app/src/main/java/com/application/gymnote/RegisterUser.java
+++ app/src/main/java/com/application/gymnote/RegisterUser.java
@@ -1,21 +1,28 @@
 package com.application.gymnote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
 
     private TextView banner, registerUser;
-    private EditText editTextName, editTextAge, editTextEmail, editTextPassword;
+    private EditText editTextName, editTextAge, editTextEmailAddress, editTextPassword;
     private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
@@ -35,7 +42,7 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
 
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextAge = (EditText) findViewById(R.id.editTextAge);
-        editTextEmail = (EditText) findViewById(R.id.editTextEmailAdress);
+        editTextEmailAddress = (EditText) findViewById(R.id.editTextEmailAddress);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -55,5 +62,70 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
     }
 
     private void registerUser() {
+        String name = editTextName.getText().toString().trim();
+        String age = editTextAge.getText().toString().trim();
+        String email = editTextEmailAddress.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(age.isEmpty()) {
+            editTextAge.setError("Your age is needed !");
+            editTextAge.requestFocus();
+            return;
+        }
+        if(name.isEmpty()) {
+            editTextName.setError("Your name is needed !");
+            editTextName.requestFocus();
+            return;
+        }
+        if(email.isEmpty()) {
+            editTextEmailAddress.setError("Your email is needed !");
+            editTextEmailAddress.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmailAddress.setError("Your email is not valid");
+            editTextEmailAddress.requestFocus();
+            return;
+        }
+        if(password.isEmpty()) {
+            editTextPassword.setError("Your email is needed !");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 8) {
+            editTextPassword.setError("Your password must be 8 characters long");
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            User user = new User(age, name, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if(task.isSuccessful()) {
+                                                Toast.makeText(RegisterUser.this, "User has been registerd successfully!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.VISIBLE);
+                                            } else {
+                                                Toast.makeText(RegisterUser.this, "User cannot be register", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(RegisterUser.this, "User cannot be register", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/application/gymnote/User.java
@@ -0,0 +1,15 @@
+package com.application.gymnote;
+
+public class User {
+    public String age, name, email;
+
+    public User(){
+
+    }
+    public User(String age, String name, String email) {
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
+
+}
